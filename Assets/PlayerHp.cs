b3a6797a7bd63ea989d3a1d2d9f5e0d7fa655f75
leeseorang <small6396@naver.com>
2023-12-03 17:29:58 +0900

    ing

---
+++ Assets/PlayerHp.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerHp : MonoBehaviour
+{
+    // Hp가 플레이어 딸아가기
+    GameObject player;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        transform.position = player.transform.position;
+    }
+}
